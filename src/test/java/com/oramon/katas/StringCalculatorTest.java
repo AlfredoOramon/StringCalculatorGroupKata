20d952acf7f8c41b4b8a40d7f3ba639ad56b8acf
alfredo.gil <alfredo.gil@oramon.nl>
2017-05-12 15:57:24 +0200

    Make surefire plugin work

---
+++ src/test/java/com/oramon/katas/StringCalculatorTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+
+/**
+ * ClassName
+ * <p>
+ * Created by Alfredo Gil Rubio
+ * <p>
+ * Date: 12/05/17 15:24
+ */
+public class StringCalculatorTest {
+
+}
---
+++ src/test/java/com/oramon/katas/TestFileTest.java
@@ -0,0 +1,21 @@
+package com.oramon.katas;
+
+import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * ClassName
+ * <p>
+ * Created by Alfredo Gil Rubio
+ * <p>
+ * Date: 12/05/17 15:01
+ */
+public class TestFileTest extends TestCase {
+
+    @Test
+    public void test()
+    {
+        //test pass activate surefire plugin
+    }
+}
